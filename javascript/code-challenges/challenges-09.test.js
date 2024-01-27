02596d5f69f8f3024e79e4f3d3e0e66e60988d3c
Isai Chaidez <isaichaidez@gmail.com>
2024-01-27 11:08:22 -0800

    finished challenge 2

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -39,7 +39,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 ------------------------------------------------------------------------------------------------ */
 
 const appendTheEnd = (str) => {
-  // Solution code here...
+  let newStr = str + ' The end.'
+  return newStr
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -133,7 +134,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should append without modifying the oiginal', () => {
     const a = 'This is my story.';
     const b = appendTheEnd(a);
