5756f38c2b9efd92dcf3aebd30fa7089fcb05ee9
Isai Chaidez <isaichaidez@gmail.com>
2024-03-29 23:47:16 -0700

    finished challenge 8

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -95,7 +95,15 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 ------------------------------------------------------------------------------------------------ */
 
 const evenOdd = (arr) => {
-     // Solution code here...
+     return arr.map((num) => {
+          if (num % 2 === 0) {
+               return "even";
+          } else if (num % 2 === 1) {
+               return "odd";
+          } else if (typeof num != "number") {
+               return "N/A";
+          }
+     });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -274,7 +282,7 @@ describe("Testing challenge 7", () => {
      });
 });
 
-xdescribe("Testing challenge 8", () => {
+describe("Testing challenge 8", () => {
      test("It should return an array containing the keys from an object", () => {
           expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([
                "odd",
