1b7dc45bf9eecdd5be26c190ad7be2d2f2dc7133
Isai Chaidez <isaichaidez@gmail.com>
2024-03-29 22:40:09 -0700

    completed challenge 3

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -29,7 +29,7 @@ Write a function named addQuestion that, given an array of strings, uses map to 
 ------------------------------------------------------------------------------------------------ */
 
 const addQuestion = (arr) => {
-     // Solution code here...
+     return arr.map((string) => string + "?");
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -212,7 +212,7 @@ describe("Testing challenge 2", () => {
      });
 });
 
-xdescribe("Testing challenge 3", () => {
+describe("Testing challenge 3", () => {
      test("It should add a question mark to the end of each string", () => {
           expect(addQuestion(["hello", "301", "students"])).toStrictEqual([
                "hello?",
