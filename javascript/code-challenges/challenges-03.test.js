e8b0c8364b4b4611f2f6e14d17c3004445adc610
Isai Chaidez <isaichaidez@gmail.com>
2024-04-01 08:42:10 -0700

    completed challenge 03

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -32,7 +32,7 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 ------------------------------------------------------------------------------------------------ */
 
 const containsAnd = (arr) => {
-     // Solution code here...
+     return arr.filter((string) => string.includes("and"));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -205,7 +205,7 @@ describe("Testing challenge 2", () => {
      });
 });
 
-xdescribe("Testing challenge 3", () => {
+describe("Testing challenge 3", () => {
      test("It should return an array of strings containing the word and", () => {
           expect(containsAnd(["panda", "ran", "and"])).toStrictEqual([
                "panda",
