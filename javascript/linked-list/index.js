96af598f1aedced97e4911c401d934f52d0e6d89
Isai Chaidez <isaichaidez@gmail.com>
2024-04-14 13:57:52 -0700

    moved linkedlist to a seperate file to help with testing

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,116 @@
-'use strict';
+"use strict";
+
+class Node {
+     constructor(value) {
+          this.value = value;
+          this.next = null;
+     }
+}
 
 class LinkedList {
+     constructor() {
+          this.head = null;
+          this.tail = null;
+     }
 
+     traverse(value) {
+          let current = this.head;
+          let values = [];
+
+          while (current) {
+               values.push(current.value);
+               if (current.value === value) {
+                    return true;
+               }
+               current = current.next;
+          }
+          if (value) {
+               return false;
+          }
+          return values.join(", ");
+     }
+
+     prepend(value) {
+          const newNode = new Node(value);
+          newNode.next = this.head;
+          this.head = newNode;
+     }
+
+     append(value) {
+          const newNode = new Node(value);
+          if (!this.head) {
+               this.head = newNode;
+               this.tail = newNode;
+          } else {
+               this.tail.next = newNode;
+               this.tail = newNode;
+          }
+     }
+
+     insertBefore(value, newValue) {
+          let current = this.head;
+          let previous = null;
+          while (current) {
+               if (current.value === value) {
+                    const newNode = new Node(newValue);
+                    if (previous) {
+                         previous.next = newNode;
+                         newNode.next = current;
+                    } else {
+                         this.prepend(newValue);
+                    }
+                    return;
+               }
+               previous = current;
+               current = current.next;
+          }
+     }
+
+     insertAfter(value, newValue) {
+          let current = this.head;
+          while (current) {
+               if (current.value === value) {
+                    const newNode = new Node(newValue);
+                    newNode.next = current.next;
+                    current.next = newNode;
+                    if (current === this.tail) {
+                         this.tail = newNode;
+                    }
+                    return;
+               }
+               current = current.next;
+          }
+     }
+
+     kthFromEnd(k) {
+          if (k < 0) {
+               return null; // Handle negative k values immediately
+          }
+
+          let fast = this.head;
+          let slow = this.head;
+          let index = 0;
+
+          // Move fast k nodes ahead first
+          while (index < k && fast !== null) {
+               fast = fast.next;
+               index++;
+          }
+
+          // If k is exactly the length of the list or fast is null when the index is still less than k
+          if (fast === null && index < k) {
+               return null; // k is greater than the number of nodes in the list
+          }
+
+          // Move both pointers until fast reaches the end of the list
+          while (fast !== null && fast.next !== null) {
+               slow = slow.next;
+               fast = fast.next;
+          }
+
+          // If fast was able to reach the end, return the value at slow
+          return fast === null ? null : slow.value;
+     }
 }
 
 module.exports = LinkedList;
