86def930b0cf9ca737d39b752541be4024aa5a95
Isai Chaidez <isaichaidez@gmail.com>
2024-06-25 10:34:02 -0700

    made the binary tree

---
+++ javascript/trees/index.js
@@ -0,0 +1,52 @@
+class Node {
+     constructor(value) {
+          this.value = value;
+          this.left = null;
+          this.right = null;
+     }
+}
+
+class BinaryTree {
+     constructor() {
+          this.root = null;
+     }
+
+     preOrder() {
+          const result = [];
+          const traverse = (node) => {
+               if (node) {
+                    result.push(node.value);
+                    traverse(node.left);
+                    traverse(node.right);
+               }
+          };
+          traverse(this.root);
+          return result;
+     }
+
+     inOrder() {
+          const result = [];
+          const traverse = (node) => {
+               if (node) {
+                    traverse(node.left);
+                    result.push(node.value);
+                    traverse(node.right);
+               }
+          };
+          traverse(this.root);
+          return result;
+     }
+
+     postOrder() {
+          const result = [];
+          const traverse = (node) => {
+               if (node) {
+                    traverse(node.left);
+                    traverse(node.right);
+                    result.push(node.value);
+               }
+          };
+          traverse(this.root);
+          return result;
+     }
+}
