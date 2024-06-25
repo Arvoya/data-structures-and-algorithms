9189f984626ccbbfdfa444f09ee92d3e1a20c439
Isai Chaidez <isaichaidez@gmail.com>
2024-06-25 14:19:45 -0700

    have some things going, but figuring out K_aryTree

--- javascript/tree-fizz-buzz/index.js
+++ javascript/tree-fizz-buzz/index.js
@@ -9,4 +9,34 @@ class K_aryTree {
      constructor() {
           this.root = null;
      }
+
+     add(value, parentValue) {
+          const newNode = new Node(value);
+          if (!this.root) {
+               this.root = newNode;
+               return;
+          }
+
+          this.add(this.root, newNode, parentValue);
+     }
+}
+
+function fizzBuzzTree(tree) {
+     if (!tree.root) {
+          return "no tree found";
+     }
+     let current = tree.root;
+     let newTree = new K_aryTree();
+
+     while (current) {
+          if (current.value % 3 == 0 && current.value % 5 == 0) {
+               newTree.add("FizzBuzz");
+          } else if (current.value % 3 == 0) {
+               newTree.add("Fizz");
+          } else if (current.value % 5 == 0) {
+               newTree.add("Buzz");
+          } else {
+               newTree.add(current.value.toString());
+          }
+     }
 }
