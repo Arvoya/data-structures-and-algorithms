686ed41c42db4a212842f6c2f9855f286183cfd1
Isai Chaidez <isaichaidez@gmail.com>
2024-06-25 14:15:33 -0700

    working on funtion, setup complete for next challenge

---
+++ javascript/tree-fizz-buzz/index.js
@@ -0,0 +1,12 @@
+class Node {
+     constructor(value) {
+          this.value = value;
+          this.children = [];
+     }
+}
+
+class K_aryTree {
+     constructor() {
+          this.root = null;
+     }
+}
